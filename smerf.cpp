d59ed4a993f016e6735a6c08f4c3bf8a9a458171
kamiljazwa <kamiljazwa@gmail.com>
2015-05-27 19:37:36 +0200

    przypisywanie współrzednych smerfom

--- smerf.cpp
+++ smerf.cpp
@@ -1,44 +1,45 @@
-    #include <iostream>
-    #include <cstdlib>
-    using namespace std;
+ #include <iostream>
+ #include <cstdlib>
+using namespace std;
+
+ class smerf
+ {
+ public:
+	int x;
+	int y;
+	void create(int xp, int yp);
      
-    class smerf
-    {
-    	public:
-    		int x;
-    		int y;
-    };
-    class klakier
-    {
-    	public:
-    		int x;
-    		int y;
-    		int range;
-    };
-    class Gargamel 
-    {
-    	public:
-    		int x;
-    		int y;
-    		int range;
-    };
+  };
+void smerf::create(int xp, int yp)
+ {
+ 	x = xp;
+   	y = yp;
+ }
+class klakier
+{
+public:
+ int x;
+int y;
+int range;
+};
+class Gargamel 
+ {
+public:
+int x;
+ int y;
+ int range;
+};
      
-    int main()
-    {
-    	smerf s1;
-    	smerf s2;
-    	smerf s3;
-    	smerf s4;
-    	smerf s5;
-    	srand( time( NULL ) );
-    	s1.x=(rand()%100);
-    	s1.y=(rand()%100);
-    	s2.x=(rand()%100);
-    	s2.y=(rand()%100);
-    	s3.x=(rand()%100);
-    	s3.y=(rand()%100);
-    	s4.x=(rand()%100);
-    	s4.y=(rand()%100);
-    	s5.x=(rand()%100);
-    	s5.y=(rand()%100);
-    }
+int main()
+{
+ 	smerf tab[10];
+ 	int i;
+  	for(i=0;i<10;i++)
+{
+   		tab[i].create((rand()%100),(rand()%100));
+}
+	for(i=0;i<10;i++)
+	{
+		cout<<tab[i].x<<"\t"<<tab[i].y<<"\n";
+	}
+}
