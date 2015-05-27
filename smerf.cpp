226679b2755a324657824a1de322114c5fd2d14f
kamiljazwa <kamiljazwa@gmail.com>
2015-05-27 19:06:51 +0200

    klasy

--- smerf.cpp
+++ smerf.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+class smerf
+{
+		public:
+			int x;
+			int y;
+}
+class klakier
+{
+		public:
+			int x;
+			int y;
+			int range;
+}
+class Gargamel 
+{
+	public:
+		int x;
+		int y;
+		int range;
+}
+	
 int main()
 {
 	
